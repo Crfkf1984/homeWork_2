7af5629548e5bc2e53cb97b310b17297a983c857
Sergey Kotovich <seirosso1@gmail.com>
2024-08-06 15:35:31 +0300

    cange in BaseTest

--- src/test/java/qa/guru/BaseTest.java
+++ src/test/java/qa/guru/BaseTest.java
@@ -7,11 +7,9 @@ public class BaseTest {
 
     @BeforeAll
     public static void before() {
-        System.setProperty("webdriver.chrome.driver", "C:\\.cache\\selenium\\chromedriver\\win64\\126.0.6478.126\\chromedriver.exe");
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
     }
 }
