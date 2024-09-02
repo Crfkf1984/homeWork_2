60087213be54c7cfee8c466ce0daf38087a062b1
Sergey Kotovich <seirosso1@gmail.com>
2024-09-02 21:55:25 +0300

    add build

--- src/test/java/qa/guru/BaseTest.java
+++ src/test/java/qa/guru/BaseTest.java
@@ -7,6 +7,7 @@ public class BaseTest {
 
     @BeforeAll
     public static void before() {
+        System.setProperty("webdriver.chrome.driver", "C:\\.cache\\selenium\\chromedriver\\win64\\126.0.6478.126\\chromedriver.exe");
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
