d11f79fdcd53060117d24d86ec3c6c4217ce8c63
Sergey Kotovich <seirosso1@gmail.com>
2024-09-04 21:15:49 +0300

    Add Attach

--- src/test/java/qa/guru/BaseTest.java
+++ src/test/java/qa/guru/BaseTest.java
@@ -1,7 +1,11 @@
 package qa.guru;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
 
@@ -12,5 +16,15 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.addVideo();
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }
--- src/test/java/qa/guru/CheckFieldsTest.java
+++ src/test/java/qa/guru/CheckFieldsTest.java
@@ -1,11 +1,15 @@
 package qa.guru;
 
 import data.TestData;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-import static data.TestData.*;
+import static io.qameta.allure.Allure.step;
 import static pages.RegistrationPage.open;
 import static pages.RegistrationPage.removeAdvertising;
 
@@ -16,74 +20,105 @@ public class CheckFieldsTest extends BaseTest {
     TestData data = new TestData();
 
     @Test
+    @DisplayName("Тест проверяет авторизацию пользователя")
+    @Owner("catovich")
+    @Severity(SeverityLevel.BLOCKER)
     public void checkAllFieldsTest() {
-        open();
-        removeAdvertising();
-        registration.setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setEmail(data.userEmail)
-                .selectGender(data.gender)
-                .setMobileNumber(data.mobileNumber)
-                .selectDate(data.day, data.month, data.year)
-                .selectSubject(data.subjectsInput)
-                .selectHobbie(data.hobbie)
-                .upploadPicture(data.picture)
-                .setAddress(data.currentAddress)
-                .selectState(data.uttarPradesh)
-                .selectCity(data.needCity).
-                clickSubmitButton();
 
-        registration.checkResult("Student Name", data.firstName + " " + data.lastName)
-                .checkResult("Student Email", data.userEmail)
-                .checkResult("Gender", data.gender)
-                .checkResult("Mobile", data.mobileNumber)
-                .checkResult("Date of Birth", data.day + " " + data.month + "," + data.year)
-                .checkResult("Subjects", data.subjectsInput)
-                .checkResult("Hobbies", data.hobbie)
-                .checkResult("Picture", data.picture)
-                .checkResult("Address", data.currentAddress)
-                .checkResult("State and City", data.uttarPradesh + " " + data.needCity);
+        step("Открываем браузер", () -> {
+            open();
+        });
+        step("Удаляем рекламу", () -> {
+            removeAdvertising();
+        });
+        step("Заполняем страницу данными", () -> {
+            registration.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setEmail(data.userEmail)
+                    .selectGender(data.gender)
+                    .setMobileNumber(data.mobileNumber)
+                    .selectDate(data.day, data.month, data.year)
+                    .selectSubject(data.subjectsInput)
+                    .selectHobbie(data.hobbie)
+                    .upploadPicture(data.picture)
+                    .setAddress(data.currentAddress)
+                    .selectState(data.uttarPradesh)
+                    .selectCity(data.needCity).
+                    clickSubmitButton();
+        });
+        step("Проверяем сохранились ли данные", () -> {
+            registration.checkResult("Student Name", data.firstName + " " + data.lastName)
+                    .checkResult("Student Email", data.userEmail)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Mobile", data.mobileNumber)
+                    .checkResult("Date of Birth", data.day + " " + data.month + "," + data.year)
+                    .checkResult("Subjects", data.subjectsInput)
+                    .checkResult("Hobbies", data.hobbie)
+                    .checkResult("Picture", data.picture)
+                    .checkResult("Address", data.currentAddress)
+                    .checkResult("State and City", data.uttarPradesh + " " + data.needCity);
+        });
+
     }
 
     @Test
+    @DisplayName("Тест проверяет авторизацию при неполном заполнении данных")
+    @Owner("catovich")
+    @Severity(SeverityLevel.BLOCKER)
     public void checkSmokeFieldsTest() {
-        open();
-        registration.setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setMobileNumber(data.mobileNumber)
-                .selectGender(data.gender)
-                .selectDate(data.day, data.month, data.year)
-                .selectSubject(data.subjectsInput)
-                .upploadPicture(data.picture)
-                .selectState(data.uttarPradesh)
-                .selectCity(data.needCity).
-                clickSubmitButton();
 
-        registration.checkResult("Student Name", data.firstName + " " + data.lastName)
-                .checkResult("Mobile", data.mobileNumber)
-                .checkResult("Gender", data.gender)
-                .checkResult("Date of Birth", data.day + " " + data.month + "," + data.year)
-                .checkResult("Subjects", data.subjectsInput)
-                .checkResult("Picture", data.picture)
-                .checkResult("State and City", data.uttarPradesh + " " + data.needCity);
+        step("Открываем браузер", () -> {
+            open();
+        });
+        step("Заполняем форму неполностью", () -> {
+            registration.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setMobileNumber(data.mobileNumber)
+                    .selectGender(data.gender)
+                    .selectDate(data.day, data.month, data.year)
+                    .selectSubject(data.subjectsInput)
+                    .upploadPicture(data.picture)
+                    .selectState(data.uttarPradesh)
+                    .selectCity(data.needCity).
+                    clickSubmitButton();
+        });
+        step("Проверяем сохранения данныех при неполной заполнении формы", () -> {
+            registration.checkResult("Student Name", data.firstName + " " + data.lastName)
+                    .checkResult("Mobile", data.mobileNumber)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Date of Birth", data.day + " " + data.month + "," + data.year)
+                    .checkResult("Subjects", data.subjectsInput)
+                    .checkResult("Picture", data.picture)
+                    .checkResult("State and City", data.uttarPradesh + " " + data.needCity);
+        });
     }
 
-    @Test
-    public void checkAllFieldsNegativeTest() {
-        open();
-        registration.setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setMobileNumber(data.mobileNumber)
-                .selectGender(data.gender)
-                .upploadPicture(data.picture)
-                .clickSubmitButton();
 
-        registration.checkResult("Student Name", data.firstName + " " + data.lastName)
-                .checkResult("Mobile", data.mobileNumber)
-                .checkResult("Gender", data.gender)
-                .checkResult("Picture", data.picture)
-                .checkResult("Subject", " ")
-                .checkResult("State and City", " ")
-                .checkResult("Address", " ");
+    @Test
+    @DisplayName("Тест проверяет авторизацию пользователя, отрицательный тест-кейс")
+    @Owner("catovich")
+    @Severity(SeverityLevel.NORMAL)
+    public void checkAllFieldsNegativeTest() {
+
+        step("Открываем браузер", () -> {
+            open();
+        });
+        step("Заполняем форму отриц данными", () -> {
+            registration.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setMobileNumber(data.mobileNumber)
+                    .selectGender(data.gender)
+                    .upploadPicture(data.picture)
+                    .clickSubmitButton();
+        });
+        step("Проверка сохранение формы при отриц данных", () -> {
+            registration.checkResult("Student Name", data.firstName + " " + data.lastName)
+                    .checkResult("Mobile", data.mobileNumber)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Picture", data.picture)
+                    .checkResult("Subject", " ")
+                    .checkResult("State and City", " ")
+                    .checkResult("Address", " ");
+        });
     }
 }
